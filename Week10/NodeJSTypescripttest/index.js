088c7dc974d1003fc778cca1834e4c1a72f5654c
LTaylor137 <61397207+LTaylor137@users.noreply.github.com>
2020-05-09 14:28:21 +1000

    week10 nodejs testing

---
+++ Week10/NodeJSTypescripttest/index.js
@@ -0,0 +1,6 @@
+var age = 5;
+age = 66;
+function main() {
+    console.log("hello" + age);
+}
+main();
---
+++ Week10/NodeJSTypescripttest/index.ts
@@ -0,0 +1,21 @@
+
+
+let age: number = 5;
+
+age = 66;
+
+function main() {
+    console.log("hello" + age)
+}
+
+
+main();
+
+
+
+
+
+
+
+
+
