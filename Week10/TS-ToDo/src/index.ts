2db1df90f2f5647488d9eae09f23dea394989dd7
LTaylor137 <61397207+LTaylor137@users.noreply.github.com>
2020-05-23 17:39:49 +1000

    fixed null checks

--- Week10/TS-ToDo/src/index.ts
+++ Week10/TS-ToDo/src/index.ts
@@ -1,59 +1,88 @@
-        // variables
-        var List = document.getElementById("List") as HTMLButtonElement | null;
-        var UserInput = document.getElementById("UserInput") as HTMLButtonElement | null;
-        var AddButton = document.getElementById("AddButton") as HTMLButtonElement | null;
-        var RemoveAll = document.getElementById("RemoveAll") as HTMLButtonElement | null;
+// variables
+let List = document.getElementById("List") as HTMLElement | null;
+let UserInput = document.getElementById("UserInput") as HTMLTextAreaElement | null;
+let AddButton = document.getElementById("AddButton") as HTMLButtonElement | null;
+let RemoveAll = document.getElementById("RemoveAll") as HTMLButtonElement | null;
 
+//add list item function
+if (AddButton == null) {
+    alert("button not found");
+} else {
+    AddButton.onclick = function Add(event) {
+        event.preventDefault();
+        console.log(AddButton);
+        if (AddButton == null) return;
 
-        //add list item function
-        AddButton.onclick = function Add() {
-
-            if(AddButton == null) return;
-
+        if (List == null) {
+            alert("button not found");
+        } else {
             var LIbox = document.createElement("div");
+            LIbox.classList.add("listitembox");
             List.append(LIbox);
-            // this.parentNode.style.align = "right";
+        
 
-            var ListItem = document.createElement("li");
-            LIbox.append(ListItem);
-            ListItem.innerHTML = UserInput.value;
-            ListItem.classList.add("list");
-            ListItem.style.color = "#00d81d";
+            if (UserInput == null) {
+                alert("input not found");
+            } else {
+                var ListItem = document.createElement("li");
+                LIbox.append(ListItem);
+                ListItem.innerHTML = UserInput.value;
+                ListItem.classList.add("listitem");
+                ListItem.style.color = "#00d81d";
 
-            //this is a cheat. couldnt work out how to justify content
-            var spacer = document.createElement("x");
-            ListItem.append(spacer);
-            spacer.innerHTML = " . . . . . . ";
-            spacer.style.color = "#FFFFFF";
+                //this is a cheat. couldnt work out how to justify content
+                var spacer = document.createElement("x");
+                ListItem.append(spacer);
+                spacer.innerHTML = " . . . . . . ";
+                spacer.style.color = "#FFFFFF";
 
-            //change colour list item function
-            ListItem.onclick = function CheckItem() {
-                ListItem.classList.add("check");
-                ListItem.style.color = "grey";
+                //change colour list item function
+                ListItem.onclick = function CheckItem() {
+                    console.log("listitemchecked")
+                    ListItem.classList.add("check");
+                    ListItem.style.color = "grey";
+                }
+
+                //adds the delete button
+                var DeleteButton = document.createElement("text");
+                spacer.append(DeleteButton);
+                DeleteButton.classList.add("listdel");
+                DeleteButton.innerHTML = "remove";
+               
+
+                //delete list item function
+                DeleteButton.onclick = function Remove() {
+                    ListItem.remove();
+                }
+
+                //resets input text box
+                if (UserInput == null) {
+                    console.log("UserInput not found");
+                } else {
+                    UserInput.value = "";
+
+                    //pauses event
+                    if (event == null) {
+                        console.log("event not found");
+                    } else {
+                        event.preventDefault();
+                    }
+
+                    // removes all items from list
+                    if (RemoveAll == null) {
+                        console.log("RemoveAll not found");
+                    } else {
+                        RemoveAll.onclick = function RemoveAll() {
+                            if (RemoveAll == null) return;
+                            if (List == null) return;
+                            List.innerHTML = "";
+                        }
+                    }
+                }
             }
-
-            //adds the delete button
-            var DeleteButton = document.createElement("text");
-            spacer.append(DeleteButton);
-            DeleteButton.innerHTML = "remove";
-            DeleteButton.style.color = "red";
-    
-
-
-            //delete list item function
-            DeleteButton.onclick = function Remove() {
-                ListItem.remove();
-            }
-
-            //resets input text box
-            UserInput.value = "";
-
-            //pauses event
-            event.preventDefault();
-
         }
+    }
+}
 
-        // removes all items from list
-        RemoveAll.onclick = function RemoveAll() {
-            List.innerHTML = "";
-        }
+
+
