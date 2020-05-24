f4aec7850747212e2f44598d75e2fc11401530c8
LTaylor137 <61397207+LTaylor137@users.noreply.github.com>
2020-05-24 13:47:58 +1000

    converted index.ts to index.js

--- Week10/TS-ToDo/src/index.js
+++ Week10/TS-ToDo/src/index.js
@@ -4,37 +4,90 @@ var UserInput = document.getElementById("UserInput");
 var AddButton = document.getElementById("AddButton");
 var RemoveAll = document.getElementById("RemoveAll");
 //add list item function
-AddButton.onclick = function Add() {
-    if (AddButton == null)
-        return;
-    var LIbox = document.createElement("div");
-    List.append(LIbox);
-    // this.parentNode.style.align = "right";
-    var ListItem = document.createElement("li");
-    LIbox.append(ListItem);
-    ListItem.innerHTML = UserInput.value;
-    ListItem.classList.add("list");
-    ListItem.style.color = "#00d81d";
-    //change colour list item function
-    ListItem.onclick = function CheckItem() {
-        ListItem.classList.add("check");
-        ListItem.style.color = "grey";
+if (AddButton == null) {
+    alert("button not found");
+}
+else {
+    AddButton.onclick = function Add(event) {
+        event.preventDefault();
+        console.log(AddButton);
+        if (AddButton == null)
+            return;
+        if (List == null) {
+            alert("button not found");
+        }
+        else {
+            var LIbox_1 = document.createElement("div");
+            LIbox_1.classList.add("listitembox");
+            List.append(LIbox_1);
+            if (UserInput == null) {
+                alert("input not found");
+            }
+            else {
+                console.log("list item created.");
+                var ListItem_1 = document.createElement("div");
+                ListItem_1.classList.add("listitem");
+                LIbox_1.append(ListItem_1);
+                ListItem_1.innerHTML = UserInput.value;
+                var checked_1 = new Boolean(false);
+                var RemoveButton_1 = document.createElement("div");
+                //change colour of list item
+                ListItem_1.onclick = function CheckItem() {
+                    //change to grey and checked
+                    if (checked_1 == false) {
+                        checked_1 = true;
+                        console.log("listitem checked");
+                        console.log("checked = " + checked_1);
+                        ListItem_1.classList.remove("listitem");
+                        ListItem_1.classList.add("checked");
+                        //adds the delete button
+                        LIbox_1.append(RemoveButton_1);
+                        RemoveButton_1.classList.add("listdel");
+                        RemoveButton_1.innerHTML = "remove";
+                        //delete list item function
+                        RemoveButton_1.onclick = function Remove() {
+                            ListItem_1.remove();
+                            RemoveButton_1.remove();
+                            LIbox_1.remove();
+                        };
+                        //change back to green
+                    }
+                    else if (checked_1 == true) {
+                        checked_1 = false;
+                        console.log("listitem unchecked");
+                        console.log("checked = " + checked_1);
+                        ListItem_1.classList.remove("checked");
+                        ListItem_1.classList.add("listitem");
+                        //removes the delete button again
+                        RemoveButton_1.remove();
+                    }
+                };
+                //resets input text box
+                if (UserInput == null) {
+                    console.log("UserInput not found");
+                }
+                else {
+                    UserInput.value = "";
+                    //pauses event
+                    if (event == null) {
+                        console.log("event not found");
+                    }
+                    else {
+                        event.preventDefault();
+                    }
+                    // removes all items from list
+                    if (RemoveAll == null) {
+                        console.log("RemoveAll not found");
+                    }
+                    else {
+                        RemoveAll.onclick = function RemoveAll() {
+                            if (List == null)
+                                return;
+                            List.innerHTML = "";
+                        };
+                    }
+                }
+            }
+        }
     };
-    //adds the delete button
-    var DeleteButton = document.createElement("text");
-    spacer.append(DeleteButton);
-    DeleteButton.innerHTML = "remove";
-    DeleteButton.style.color = "red";
-    //delete list item function
-    DeleteButton.onclick = function Remove() {
-        ListItem.remove();
-    };
-    //resets input text box
-    UserInput.value = "";
-    //pauses event
-    event.preventDefault();
-};
-// removes all items from list
-RemoveAll.onclick = function RemoveAll() {
-    List.innerHTML = "";
-};
+}
